c72ce75cdd88b48c41a2187b699c5f9c4cf3e25d
Can Vardar <ismailcanvardar@gmail.com>
2024-06-09 14:41:40 +0300

    refactor(utils): update token enum

--- utils/src/token.rs
+++ utils/src/token.rs
@@ -93,305 +93,480 @@ fn switch_mode(lex: &mut Lexer<Token>) {
     };
 }
 
+fn check_reserve_word(lex: &mut Lexer<Token>) -> Token {
+    let cur_token = lex.slice();
+
+    match cur_token.as_bytes() {
+        b"case" => {
+            if lex.extras.mode != Mode::Yul {
+                return Token::ReservedKeyword;
+            }
+
+            return Token::YulCase;
+        }
+        _ => {
+            return Token::Illegal;
+        }
+    }
+}
+
 #[derive(Logos, Debug, PartialEq, Clone)]
 #[logos(extras = LexerState)]
 pub enum Token {
     #[end]
     Eof,
+
     Illegal,
-    #[regex(
-        r"after|alias|apply|auto|byte|case|copyof|default|define|final|implements|in|inline|let|macro|match|mutable|null|of|partial|promise|reference|relocatable|sealed|sizeof|static|supports|switch|typedef|typeof|var",
-        |lex| lex.slice().to_string()
-    )]
-    ReservedKeyword(String),
+
+    #[regex(r"after|alias|apply|auto|byte|copyof|default|define|final")]
+    #[regex(r"implements|in|inline|let|macro|match|mutable|null|of|partial")]
+    #[regex(r"promise|reference|relocatable|sealed|sizeof|static|supports")]
+    #[regex(r"switch|typedef|typeof|var")]
+    ReservedKeyword,
+
     #[token("abstract")]
     Abstract,
+
     #[token("address")]
     Address,
+
     #[token("anonymous")]
     Anonymous,
+
     #[token("as")]
     As,
+
     #[token("assembly", switch_mode)]
     Assembly,
+
     #[token("bool")]
     Bool,
+
     #[token("break", switch_mode)]
     Break,
+
     #[token("bytes")]
     Bytes,
+
     #[token("calldata")]
     Calldata,
+
     #[token("catch")]
     Catch,
+
     #[token("constant")]
     Constant,
+
     #[token("constructor")]
     Constructor,
+
     #[token("continue")]
     Continue,
+
     #[token("contract")]
     Contract,
+
     #[token("delete")]
     Delete,
+
     #[token("do")]
     Do,
+
     #[token("else")]
     Else,
+
     #[token("emit")]
     Emit,
+
     #[token("enum")]
     Enum,
+
     #[token("error")]
     Error,
+
     #[token("event")]
     Event,
+
     #[token("external")]
     External,
+
     #[token("fallback")]
     Fallback,
+
     #[token("false")]
     False,
+
     #[token("fixed")]
     Fixed,
+
     #[regex(r"fixed[1-9][0-9]*x[1-9][0-9]*")]
     FixedWithSize,
+
     #[regex(r"bytes[1-9]|bytes[1-2][0-9]|bytes3[0-2]")]
     FixedBytes,
+
     #[token("for")]
     For,
+
     #[token("from")]
     From,
+
     #[token("function")]
     Function,
+
     #[token("global")]
     Global,
+
     #[token("hex")]
     Hex,
+
     #[token("if")]
     If,
+
     #[token("immutable")]
     Immutable,
+
     #[token("import")]
     Import,
+
     #[token("indexed")]
     Indexed,
+
     #[token("interface")]
     Interface,
+
     #[token("internal")]
     Internal,
+
     #[token("is")]
     Is,
+
     #[token("library")]
     Library,
+
     #[token("mapping")]
     Mapping,
+
     #[token("memory")]
     Memory,
+
     #[token("modifier")]
     Modifier,
+
     #[token("new")]
     New,
+
     #[regex(r"wei|gwei|ether|seconds|minutes|hours|days|weeks|years")]
     SubDenomination,
+
     #[token("override")]
     Override,
+
     #[token("payable")]
     Payable,
+
     #[token("pragma", switch_mode)]
     Pragma,
+
     #[token("private")]
     Private,
+
     #[token("public")]
     Public,
+
     #[token("pure")]
     Pure,
+
     #[token("receive")]
     Receive,
+
     #[token("require")]
     Require,
+
     #[token("return")]
     Return,
+
     #[token("returns")]
     Returns,
+
     #[token("revert")]
     Revert,
-    #[regex(r"int|int8|int16|int24|int32|int40|int48|int56|int64|int72|int80|int88|int96|int104|int112|int120|int128|int136|int144|int152|int160|int168|int176|int184|int192|int200|int208|int216|int224|int232|int240|int248|int256")]
+
+    #[regex("int(8|16|24|32|40|48|56|64|72|80|88|96|104|112|120|128|136|144)")]
+    #[regex("int(152|160|168|176|184|192|200|208|216|224|232|240|248|256)")]
     SignedIntegerType,
+
     #[token("storage")]
     Storage,
+
     #[token("string")]
     String,
+
     #[token("struct")]
     Struct,
+
     #[token("super")]
     Super,
+
     #[token("this")]
     This,
+
     #[token("true")]
     True,
+
     #[token("try")]
     Try,
+
     #[token("type")]
     Type,
+
     #[token("ufixed")]
     Ufixed,
+
     #[regex(r"ufixed[1-9][0-9]*x[1-9][0-9]*")]
     UfixedWithSize,
+
     #[token("unchecked")]
     Unchecked,
+
     #[token("unicode")]
     Unicode,
-    #[regex(r"uint|uint8|uint16|uint24|uint32|uint40|uint48|uint56|uint64|uint72|uint80|uint88|uint96|uint104|uint112|uint120|uint128|uint136|uint144|uint152|uint160|uint168|uint176|uint184|uint192|uint200|uint208|uint216|uint224|uint232|uint240|uint248|uint256")]
+
+    #[regex("uint(8|16|24|32|40|48|56|64|72|80|88|96|104|112|120|128|136|144)")]
+    #[regex("uint(152|160|168|176|184|192|200|208|216|224|232|240|248|256)")]
     UnsignedIntegerType,
+
     #[token("using")]
     Using,
+
     #[token("view")]
     View,
+
     #[token("virtual")]
     Virtual,
+
     #[token("while")]
     While,
+
     #[token("(")]
     LParen,
+
     #[token(")")]
     RParen,
+
     #[token("[")]
     LBrack,
+
     #[token("]")]
     RBrack,
+
     #[token("{", switch_mode)]
     LBrace,
+
     #[token("}", switch_mode)]
     RBrace,
+
     #[token(":")]
     Colon,
+
     #[token(";", switch_mode)]
     Semicolon,
+
     #[token(".")]
     Period,
+
     #[token("?")]
     Conditional,
+
     #[token("=>")]
     DoubleArrow,
+
     #[token("->")]
     RightArrow,
+
     #[token("=")]
     Assign,
+
     #[token("|=")]
     AssignBitOr,
+
     #[token("^=")]
     AssignBitXor,
+
     #[token("&=")]
     AssignBitAnd,
+
     #[token("<<=")]
     AssignShl,
+
     #[token(">>=")]
     AssignSar,
+
     #[token(">>>=")]
     AssignShr,
+
     #[token("+=")]
     AssignAdd,
+
     #[token("-=")]
     AssignSub,
+
     #[token("*=")]
     AssignMul,
+
     #[token("/=")]
     AssignDiv,
+
     #[token("%=")]
     AssignMod,
+
     #[token(",")]
     Comma,
+
     #[token("||")]
     Or,
+
     #[token("&&")]
     And,
+
     #[token("|")]
     BitOr,
+
     #[token("^")]
     BitXor,
+
     #[token("&")]
     BitAnd,
+
     #[token("<<")]
     Shl,
+
     #[token(">>")]
     Sar,
+
     #[token(">>>")]
     Shr,
+
     #[token("+")]
     Add,
+
     #[token("-")]
     Sub,
+
     #[token("*")]
     Mul,
+
     #[token("/")]
     Div,
+
     #[token("%")]
     Mod,
+
     #[token("**")]
     Exp,
+
     #[token("==")]
     Equal,
+
     #[token("!=")]
     NotEqual,
+
     #[token("<")]
     LessThan,
+
     #[token(">")]
     GreaterThan,
+
     #[token("<=")]
     LessThanOrEqual,
+
     #[token(">=")]
     GreaterThanOrEqual,
+
     #[token("!")]
     Not,
+
     #[token("~")]
     BitNot,
+
     #[token("++")]
     Inc,
+
     #[token("--")]
     Dec,
+
     #[regex("\"")]
     DoubleQuote,
+
     #[regex("'")]
     SingleQuote,
+
     #[regex("\"(\\\"|[^\"\\\\])*\"", priority = 5)]
     NonEmptyStringLiteral,
+
     #[regex("\"\"")]
     EmptyStringLiteral,
+
     #[regex("'(\\\\'|[^'\\\\])*'")]
     UnicodeStringLiteral,
+
     #[regex("hex(\"(\\\\\"|[^\"\\\\])*\"|'(\\\\'|[^'\\\\])*')")]
     HexString,
+
     #[regex("0x[0-9a-fA-F]+")]
     HexNumber,
+
     #[regex("'0'[0-7]+(\\.[0-9]+)?")]
     OctalNumber,
-    #[regex("0[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?|[1-9][0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?|0|([1-9][0-9]*([eE][-+]?[0-9]+)?)")]
-    DecimalNumber,
-    #[regex("(0[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?|[1-9][0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?|0|([1-9][0-9]*([eE][-+]?[0-9]+)?)) [a-zA-Z$_][a-zA-Z0-9$_]*")]
-    DecimalNumberFollowedByIdentifier,
+
+    #[regex("[0-9]+")]
+    Integer,
+
+    #[regex(r"[0-9]*\\.[0-9]+([eE][+-]?[0-9]+)?|[0-9]+[eE][+-]?[0-9]+")]
+    Rational,
+
     #[regex("[a-zA-Z$_][a-zA-Z0-9$_]*")]
     Identifier,
-    #[regex("[ \\t\\r\\n\\u{000C}]+", logos::skip)]
-    Whitespace,
-    #[regex("/\\*([^*]|\\*[^/])*\\*/")]
-    Comment,
-    #[regex("//.*")]
-    LineComment,
-    #[token("case")]
+
+    #[token("case", check_reserve_word)]
     YulCase,
+
     #[token("default")]
     YulDefault,
+
     #[token("leave")]
     YulLeave,
+
     #[token("let")]
     AssemblyLet,
+
     #[token("switch")]
     AssemblySwitch,
-    #[regex("stop|add|sub|mul|div|sdiv|mod|smod|exp|not|lt|gt|slt|sgt|eq|iszero|and|or|xor|byte|shl|shr|sar|addmod|mulmod|signextend|keccak256|pop|mload|mstore|mstore8|sload|sstore|tload|tstore|msize|gas|address|balance|selfbalance|caller|callvalue|calldataload|calldatasize|calldatacopy|extcodesize|extcodecopy|returndatasize|returndatacopy|mcopy|extcodehash|create|create2|call|callcode|delegatecall|staticcall|return|revert|selfdestruct|invalid|log0|log1|log2|log3|log4|chainid|origin|gasprice|blockhash|blobhash|coinbase|timestamp|number|difficulty|prevrandao|gaslimit|basefee|blobbasefee", priority=5)]
+
+    #[regex("stop|add|sub|mul|div|sdiv|mod|smod|exp|not|lt|gt|slt|sgt|eq|iszero|and|or")]
+    #[regex("xor|byte|shl|shr|sar|addmod|mulmod|signextend|keccak256|pop|mload|mstore|mstore8")]
+    #[regex("sload|sstore|tload|tstore|msize|gas|address|balance|selfbalance|caller|callvalue")]
+    #[regex("calldataload|calldatasize|calldatacopy|extcodesize|extcodecopy|returndatasize")]
+    #[regex("returndatacopy|mcopy|extcodehash|create|create2|call|callcode|delegatecall")]
+    #[regex("staticcall|return|revert|selfdestruct|invalid|log0|log1|log2|log3|log4|chainid")]
+    #[regex("origin|gasprice|blockhash|blobhash|coinbase|timestamp|number|difficulty|prevrandao")]
+    #[regex("gaslimit|basefee|blobbasefee")]
     AssemblyBuiltin,
-    #[token(":=")]
+
+    #[token("=:")]
     AssemblyAssign,
+
+    #[token(":=")]
+    AssemblyBind,
+
+    #[regex("[ \\t\\r\\n\\u{000C}]+", logos::skip)]
+    Whitespace,
+
+    #[regex("/\\*([^*]|\\*[^/])*\\*/")]
+    Comment,
+
+    #[regex("//.*")]
+    LineComment,
 }
 
 #[cfg(test)]
@@ -402,11 +577,10 @@ mod tests {
     fn test_pragma_mode() {
         let mut lex = Token::lexer("pragma solidity ^0.8.16;");
 
-        let tests = vec![
+        let _tests = vec![
             (Token::Pragma, Mode::Pragma, "pragma"),
             (Token::Identifier, Mode::Pragma, "solidity"),
             (Token::BitXor, Mode::Pragma, "^"),
-            (Token::DecimalNumber, Mode::Pragma, "0.8"),
             (Token::Period, Mode::Pragma, "."),
         ];
 
